e62418318194cb64349cbc8ae8c1bda0451a8a48
ErtelDG <ertel.developer@t-online.de>
2024-02-10 11:11:05 +0100

    1707559827243

--- structures/structures.go
+++ structures/structures.go
@@ -5,17 +5,18 @@ type Contact struct {
 	First_Name string `json:"first_name"`
 	Last_Name  string `json:"last_name"`
 	Email      string `json:"email"`
-	Phone      int    `json:"phone"`
+	Phone      string `json:"phone"`
 }
 
 type Task struct {
 	ID_task     string
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Assigned    string `json:"assigned"`
-	Prio        string `json:"prio"`
-	Due_date    string `json:"due_date"`
-	Category    string `json:"category"`
+	Status      string   `json:"status"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Assigned    string   `json:"assigned"`
+	Prio        string   `json:"prio"`
+	Due_date    string   `json:"due_date"`
+	Category    string   `json:"category"`
 	Subtasks    []string `json:"subtasks"`
 }
 
