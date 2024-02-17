00a047c41d6a2b7440936274120231f83cbdc819
ErtelDG <ertel.developer@t-online.de>
2024-02-17 11:51:06 +0100

    1708166878056

--- structures/structures.go
+++ structures/structures.go
@@ -1,3 +1,16 @@
+// The code defines Go structs for Contact, Task, and Subtask with specified fields and JSON tags.
+// @property {string} ID_contact - The `ID_contact` field in the `Contact` struct represents the unique
+// identifier for a contact.
+// @property {string} First_Name - The `First_Name` field is a string type in the `Contact` struct with
+// the JSON tag `first_name`. It is used to store the first name of a contact in a contact management
+// system.
+// @property {string} Last_Name - Last_Name is a field in the Contact struct that represents the last
+// name of a contact. It is of type string and is tagged with `json:"last_name"` for JSON marshaling
+// and unmarshaling.
+// @property {string} Email - The `Email` property is a field in the `Contact` struct. It represents
+// the email address of a contact.
+// @property {string} Phone - The `Phone` property is a field in the `Contact` struct. It represents
+// the phone number of a contact.
 package structures
 
 type Contact struct {
@@ -25,4 +38,3 @@ type Subtask struct {
     Title     string `json:"title"`
     Checked   bool   `json:"checked"`
 }
-
